6b5cb8c10d16de990eff59a3f60b11500e33fe95
Vipin Yadav <vipin@Vipins-MacBook-Air.local>
2025-10-28 00:03:23 +0530

    heap sort

---
+++ sortings/heap-sort/heapsort.py
@@ -0,0 +1,43 @@
+arr = [9, 4, 3, 8, 10, 2, 5]
+
+# Function to perform heapify
+# n: size of heap
+# i: index of root element
+
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    left = 2 * i + 1     # left = 2*i + 1
+    right = 2 * i + 2    # right = 2*i + 2
+
+    # See if left child of root exists and is greater than root
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    # See if right child of root exists and is greater than root
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    # Change root, if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+
+        # Heapify the root.
+        heapify(arr, n, largest)
+
+
+# Main function to do heap sort
+def heap_sort(arr):
+    n = len(arr)
+
+    # Build a maxheap.
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    # One by one extract elements from heap
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # swap
+        heapify(arr, i, 0)
+
+
+heap_sort(arr)
+print("Sorted array is:", arr)
